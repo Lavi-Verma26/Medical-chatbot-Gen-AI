8c90ecc472322c92e47159d8121279171d404a24
Lavi Verma <lavi.verma2615@gmail.com>
2025-09-14 10:59:50 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot_genai',
+    version='0.1.0',
+    author='Lavi Verma',
+    author_email='lavi.verma2615@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
